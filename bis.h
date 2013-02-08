cbd0b24c0f803e61a75e3e1b6e83813f2d6e827e
Jon <jonathan-hamm@utulsa.edu>
2013-02-07 20:28:12 -0600

    test

--- bis.h
+++ bis.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 
-#define GET_CHROMBUFFER(pool) ((uint64_t *)(pool->child + sizeof(pool_s *)))
-#define GET_CHROMBYTESIZE(pool) ((pool->chromsize >> 3) + \
+#define _POOLSIZE 50
+#define _GET_CHRBYSIZE(pool) ((pool->chromsize >> 3) + \
                                   (pool->chromsize % 8 != 0))
 
 typedef struct pool_s pool_s;
@@ -13,12 +13,10 @@ struct pool_s
 {
   uint32_t gen;       /*generation number*/
   uint16_t chromsize; /*size in bits*/
-  uint16_t popsize;   /*number of chromosomes*/
   uint64_t cmask      /*bit mask for each chromosome*/
   pool_s *parent;
   pool_s *child;
-  /*Allocated at Runtime:*/
-  /*uint64_t chromosomes[...]*/ 
+  uint64_t *popul;
 };
 
 
